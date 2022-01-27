f7e9057f2da5effaf36335a78e084eaabc1d32a5
Gabriel <gabrielalejandro.br99@gmail.com>
2022-01-26 19:48:59 -0600

    Libraries and  required tools
    Most of the required  libraries for the developement of the  project werea dded into the app. everything seems to work correctly

--- LocalFood/App.js
+++ LocalFood/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Store } from './src/Others/redux/store.js';
-import { TestScreen } from './src/Screens/TestScreen.js';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Home, Login, SignUp, Confirmation, Cart } from './src/Screens/index'
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const screenOptions = {
+    headerShown: false,
+    headerStyle: {
+      elevation: 0,
+      shadowOpacity: 0,
+    },
+    headerTintColor: '#5974f5', 
+    title: '',
+  }
+
   return (
     <Provider store={Store}>
-      <TestScreen />
+      <NavigationContainer>
+        {
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} options={screenOptions} />
+            <Stack.Screen name="Login" component={Login} options={screenOptions} />
+            <Stack.Screen name="SignUp" component={SignUp} options={screenOptions} />
+            <Stack.Screen name="Confirmation" component={Confirmation} options={screenOptions} />
+            <Stack.Screen name="Cart" component={Cart} options={screenOptions} />
+          </Stack.Navigator>
+        }
+      </NavigationContainer>
     </Provider>
   );
 };
