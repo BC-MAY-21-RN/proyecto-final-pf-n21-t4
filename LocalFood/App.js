eeff01e9f77184746ce25b20c5014cd25a25a9d0
Javier_Barreto <javierbarretocp@gmail.com>
2022-01-28 17:56:57 -0600

    Fixed something

--- LocalFood/App.js
+++ LocalFood/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Store } from './src/Others/redux/store.js';
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home, Login, SignUp, Confirmation, Cart } from './src/Screens/index'
-
-const Stack = createNativeStackNavigator();
 
 const App = () => {
   const screenOptions = {
