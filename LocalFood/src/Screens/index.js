f73c54e86343ba29bd3abf8775434359ed082d98
Javier Anastasio <60673812+Javier-Barreto@users.noreply.github.com>
2022-01-31 17:18:14 -0600

    Delete LocalFood directory

--- LocalFood/src/Screens/index.js
+++
@@ -1,5 +0,0 @@
-export { Home } from './Home/Home'
-export { Login } from './Login/Login'
-export { SignUp } from './SignUp/SignUp'
-export { Confirmation } from './Confirmation/Confirmation'
-export { Cart } from './Cart/Cart'
