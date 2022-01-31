44adf2ec27da5ed728ac114eedae5270623f0085
Gabriel <gabrielalejandro.br99@gmail.com>
2022-01-31 02:18:18 -0600

    Added libraries
    Must add Redux Integration

--- LocalFoodApp/src/Screens/Login/Login.js
+++ LocalFoodApp/src/Screens/Login/Login.js
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export const Login = () => {
   return (
     <View>
-      <Text>Login</Text>
+      <Text style={styles.Text}>Login</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Text:{
+    padding: 20,
+    color: 'black',
+  }
+});
