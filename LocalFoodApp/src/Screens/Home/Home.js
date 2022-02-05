16d90c26b1bd46c09e836f4818042d45811f003f
Gabriel <gabrielalejandro.br99@gmail.com>
2022-02-05 01:09:15 -0600

    carousel working
    the carousel scrolls  thruogh an array of objects passed as props in the component
    
    todos. add aniimation/transition
    
    Co-Authored-By: Javier Anastasio <60673812+Javier-Barreto@users.noreply.github.com>
    Co-Authored-By: Roberto Faustino Carrasco Medina <79160057+RobertoCarMed@users.noreply.github.com>

--- LocalFoodApp/src/Screens/Home/Home.js
+++ LocalFoodApp/src/Screens/Home/Home.js
@@ -19,12 +19,20 @@ import ShopCard from '../../Components/ShopCad/ShopCard';
 const shops = [
   {
     name: 'Dominos Pizza',
-    image: 'https://static.onecms.io/wp-content/uploads/sites/35/2019/02/03192047/processed-food-fb.jpg',
+    image: 'https://w6h5a5r4.rocketcdn.me/wp-content/uploads/2019/06/pizza-con-chorizo-jamon-y-queso-1080x671.jpg',
   },
   {
     name: 'shop two',
-    image: 'https://static.onecms.io/wp-content/uploads/sites/35/2019/02/03192047/processed-food-fb.jpg'
-  }
+    image: 'https://static.onecms.io/wp-content/uploads/sites/35/2019/02/03192047/processed-food-fb.jpg',
+  },
+  {
+    name: 'shop three',
+    image: 'https://s3.amazonaws.com/prod.tctmd.com/public/2021-03/Eating%20More%20Ultraprocessed%20%E2%80%98Junk%E2%80%99%20Food%20Linked%20to%20Higher%20CVD%20Risk.jpeg',
+  },
+  {
+    name: 'shop four',
+    image: 'https://images.squarespace-cdn.com/content/v1/53b839afe4b07ea978436183/1608506169128-S6KYNEV61LEP5MS1UIH4/traditional-food-around-the-world-Travlinmad.jpg',
+  },
 ]
 
 export const Home = () => {
@@ -36,7 +44,7 @@ export const Home = () => {
           <InputComponent Tipo={'Busqueda'} inputPlaceHolder='Que se te antoja hoy?' hasLabel={false}/>
           <Title text={"Los más pedidos de la semana"} lineBelow={false}/>
           
-          <Carousel shops={shops}/>
+          <Carousel shops={shops} timer={3000}/>
 
           <Title text={"Recien añadidos"} lineBelow={true}/>
           {shops?.map((tienda, index) => <ShopCard key={index} shop={tienda}/>)}
